71f0acb28346b32f32e3e273a8ac65603bb8b933
Walter Huang <autumnhao@gmail.com>
2021-04-03 14:18:56 -0700

    Add new function so that user can add new list items.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 app.set('view engine', 'ejs');
 
+let inputItems = ['Food to buy', 'Things Want To Do', 'Thingd To Do'];
+
 app.get('/', (req, res) => {
 
   let today = new Date();
@@ -16,7 +18,14 @@ app.get('/', (req, res) => {
   }
 
   let day = today.toLocaleDateString('en-US', options);
-  res.render('list', { kindOfDay: day });
+  res.render('list', { kindOfDay: day, newListItems: inputItems });
+
+});
+
+app.post('/', (req, res) => {
+  
+  inputItems.push(req.body.newItem);
+  res.redirect('/');
 
 });
 
